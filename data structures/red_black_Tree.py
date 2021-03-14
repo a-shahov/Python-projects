7eb66466d6b2d20ff6a8e75e60c34517db48f0d5
Andrey-Sha <shaxov.00002@mail.ru>
2021-03-14 18:48:19 +0500

    working method pop

--- data structures/red_black_Tree.py
+++ data structures/red_black_Tree.py
@@ -84,6 +84,36 @@ class Tree:
 				tmp = tmp.left
 
 
+	def _find_min(self, tmp):
+		while True:
+			if tmp.left is None:
+				return tmp
+			tmp = tmp.left
+
+
+	def get_min(self):
+		assert self.root, "Tree is empty!" 
+		tmp = self.root
+		while True:
+			if tmp.left is None:
+				return tmp.key, tmp.value
+			tmp = tmp.left
+
+	
+	def get_max(self):
+		assert self.root, "Tree is empty!" 
+		tmp = self.root
+		while True:
+			if tmp.right is None:
+				return tmp.key, tmp.value
+			tmp = tmp.right
+
+
+	def clear(self):
+		self.root = None
+		self.size = 0
+
+
 	def push(self, key, value=None):
 		x = self._create_node(key, value)
 		if self.root is None:
@@ -139,81 +169,57 @@ class Tree:
 		assert tmp is not None
 		couple = (tmp.key, tmp.value)
 		self.size -= 1
-		if tmp.right is None: #There is no right subtree
-			if tmp.left is None: #The element to remove (tmp) - sheet
-				#No need to change coefficients skew, h_r_tree, h_l_tree
-				if tmp.parent is None: #The removed item is the root, size = 1
+		if tmp.right is None:
+			if tmp.left is None:
+				if tmp.parent is None:
 					self.clear()
-					tmp_transit = None #For function __count_skew_delete
-					orientation = "None" #For function __count_skew_delete
-				#It is necessary to change the coefficients skew, h_r_tree, h_l_tree
-				else: #The element to be removed (tmp) has a parent
-					#we come to the left, that is, +1 to skew, at tmp.parent
+				else:
 					if tmp.parent.left is tmp:
 						tmp.parent.left = None
-						orientation = "left"
-					#we come to the right, that is, -1 to skew, for tmp.parent
 					elif tmp.parent.right is tmp:
 						tmp.parent.right = None
-						orientation = "right"
-					tmp_transit = tmp.parent
-			else: #The item being removed(tmp) has a left child
-				#No need to change coefficients skew, h_r_tree, h_l_tree
-				if tmp.parent is None: #The removed item(tmp) is the root, size = 2
+			else:
+				if tmp.parent is None:
 					tmp.left.parent = None
 					self.root = tmp.left
-					tmp_transit = None #For function __count_skew_delete
-					orientation = "None"
-				#It is necessary to change the coefficients skew, h_r_tree, h_l_tree
-				else: #The element to be removed(tmp) has a parent
-					#we come to the left, that is, +1 to skew, at tmp.parent
+				else:
 					if tmp.parent.left is tmp:
 						tmp.parent.left = tmp.left
-						orientation = "left"
-					#we come to the right, that is, -1 to skew, for tmp.parent
+						tmp.left.parent = tmp.parent
 					elif tmp.parent.right is tmp:
 						tmp.parent.right = tmp.left
-						orientation = "right"
-					tmp.left.parent = tmp.parent
-					tmp_transit = tmp.parent
-		else: #The item being removed(tmp) has a right subtree
-			tmp_min = self.__find_min(tmp)
+						tmp.left.parent = tmp.parent
+		else:
+			tmp_min = self._find_min(tmp.right)
 			if tmp_min.parent is tmp:
-				#we come to the right, that is, -1 to skew, to tmp_min(after reassignments)
-				if tmp.parent is None: #The item to remove(tmp) is the root
+				if tmp.parent is None:
 					tmp_min.parent = None
+					tmp_min.left = tmp.left
+					if tmp.left is not None:
+						tmp.left.parent = tmp_min
 					self.root = tmp_min
-				else: #The element to be removed(tmp) has a parent
+				else:
 					tmp_min.parent = tmp.parent
+					tmp_min.left = tmp.left
+					if tmp.left is not None:
+						tmp.left.parent = tmp_min
 					if tmp.parent.right is tmp:
 						tmp.parent.right = tmp_min
 					elif tmp.parent.left is tmp:
 						tmp.parent.left = tmp_min
-						
-				if tmp.left is not None:
-					tmp.left.parent = tmp_min
-				tmp_min.left = tmp.left
-				
-				#tmp_min replaces tmp and gets its old attributes
-				#which will be adjusted in __count_skew_delete
-				tmp_min.skew = tmp.skew
-				tmp_min.h_r_tree = tmp.h_r_tree
-				tmp_min.h_l_tree = tmp.h_l_tree
-				
-				orientation = "right"
-				tmp_transit = tmp_min
 			else:
-				#we come to the left, that is, +1 to skew, in tmp_min.parent(before reassignments)
-				orientation = "left"
-				tmp_transit = tmp_min.parent
-				
-				if tmp.parent is None: #The item to remove (tmp) is the root
+				if tmp.parent is None:
 					tmp_min.parent.left = tmp_min.right
 					if tmp_min.right is not None:
 						tmp_min.right.parent = tmp_min.parent
 					tmp_min.parent = None
+					tmp.right.parent = tmp_min
+					tmp_min.right = tmp.right
+					tmp_min.left = tmp.left
+					if tmp.left is not None:
+						tmp.left.parent = tmp_min
 					self.root = tmp_min
-				else: #The element to be removed (tmp) has a parent
+				else:
 					tmp_min.parent.left = tmp_min.right
 					if tmp_min.right is not None:
 						tmp_min.right.parent = tmp_min.parent
@@ -222,19 +228,12 @@ class Tree:
 						tmp.parent.right = tmp_min
 					elif tmp.parent.left is tmp:
 						tmp.parent.left = tmp_min
-						
-				tmp.right.parent = tmp_min
-				tmp_min.right = tmp.right
-				#tmp.left is not None because tmp has a right subtree
-				#which consists of at least two elements
-				tmp.left.parent = tmp_min
-				tmp_min.left = tmp.left
-		
-		self._balance_pop(tmp_transit, orientation)
+					tmp.right.parent = tmp_min
+					tmp_min.right = tmp.right
+					tmp_min.left = tmp.left
+					if tmp.left is not None:
+						tmp.left.parent = tmp_min
 		return couple
-
-
-
 
 
 A = Tree()
@@ -249,4 +248,5 @@ A.push(17)
 A.push(18)
 A.push(19)
 print(A)
-
+A.pop(4)
+print(A)
